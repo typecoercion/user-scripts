b256349d2f8c0efb1b5affafdad77c09f7615cb1
Carbide Mouse <carbidemouse@gmail.com>
2022-11-23 20:12:42 -0500

    user-scripts

---
+++ clickup.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         ClickUp
+// @icon         https://app-cdn.clickup.com/assets/favicons/apple-touch-icon.png
+// @downloadURL  https://raw.githubusercontent.com/typecoercion/user-scripts/main/clickup.js
+// @namespace    http://tampermonkey.net/
+// @version      0.0.3
+// @description  TC
+// @author       TC
+// @homepage     https://gist.github.com/garthkerr/d2e21a7f58a3fa413c59d43c2bc6c70e
+// @include      https://app.clickup.com/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
+
+GM_addStyle(``);
