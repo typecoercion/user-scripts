eef6d232206240e434ad54fb4763ccbbc59d868c
Carbide Mouse <carbidemouse@gmail.com>
2022-11-23 19:26:40 -0500

    user-scripts

---
+++ aws.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         AWS
+// @icon         https://a0.awsstatic.com/libra-css/images/site/touch-icon-ipad-144-smile.png
+// @downloadURL  https://raw.githubusercontent.com/typecoercion/user-scripts/main/aws.js
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  TC
+// @author       TC
+// @homepage     https://github.com/typecoercion/user-scripts
+// @match        https://*.console.aws.amazon.com/*
+// @grant        GM_addStyle
+// @run-at       document-idle
+// ==/UserScript==
+
+GM_addStyle(`
+
+#f {
+  display: none !important;
+}
+
+`);
