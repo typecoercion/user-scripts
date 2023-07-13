23971b043da62dc741e14f6944a271a9f8c97680
Carbide Mouse <carbidemouse@gmail.com>
2023-07-13 14:36:02 -0400

    user-scripts

--- outlook.js
+++ outlook.js
@@ -3,7 +3,7 @@
 // @icon         https://res-1.cdn.office.net/assets/mail/pwa/v1/pngs/Outlook.512x512.png
 // @downloadURL  https://raw.githubusercontent.com/typecoercion/user-scripts/main/outlook.js
 // @namespace    http://tampermonkey.net/
-// @version      1.0.9
+// @version      1.1.0
 // @description  TC
 // @author       TC
 // @homepage     https://github.com/typecoercion/user-scripts
