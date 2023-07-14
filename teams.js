751ec149c107eed9acad0e472103346ebb22118b
Carbide Mouse <carbidemouse@gmail.com>
2023-07-14 12:52:55 -0400

    user-scripts

--- teams.js
+++ teams.js
@@ -4,7 +4,7 @@
 // @icon         https://statics.teams.cdn.office.net/hashed/favicon/prod/favicon-96x96-72d9855.png
 // @downloadURL  https://raw.githubusercontent.com/typecoercion/user-scripts/main/teams.js
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  TC
 // @author       TC
 // @homepage     https://github.com/typecoercion/user-scripts
@@ -16,19 +16,19 @@
 GM_addStyle(`
 
 #chatstab .left-rail-item-hover {
-    display: none;
+  display: none;
 }
 
 #chatstab div.left-rail-selected {
-    display: block;
+  display: block;
 }
 
 #chatstab div.left-rail-item-hover:has(> .ts-unread-channel) {
-    display: block;
+  display: block;
 }
 
 #chatstab div[data-tid="pinned-chat-list"] div.left-rail-item-hover {
-    display: block;
+  display: block;
 }
 
 channel-list img.team-icon {
@@ -41,7 +41,23 @@ channel-list profile-picture:after {
   margin: 0px 0 3px 0;
   opacity: 0.5;
   vertical-align: top;
-  width: 16px;
+  width: 15px;
+}
+
+channel-list .ts-channel-list a.channel-anchor {
+  padding-left: 3.6rem;
+}
+
+channel-list h3.channel-list-team-header {
+  font-size: 12px;
+}
+
+channel-list .channel-list-channels {
+  font-size: 0.8em;
+}
+
+div.ts-title-bar-icon {
+  display: none !important;
 }
 
 `)
