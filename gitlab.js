421a9fa8f2f175ee0f43efd5cca2afbe4b13ce72
Carbide Mouse <carbidemouse@gmail.com>
2023-07-27 12:48:51 -0400

    user-scripts

--- gitlab.js
+++ gitlab.js
@@ -3,7 +3,7 @@
 // @icon         https://assets.gitlab-static.net/assets/msapplication-tile-1196ec67452f618d39cdd85e2e3a542f76574c071051ae7effbfde01710eb17d.png
 // @downloadURL  https://raw.githubusercontent.com/typecoercion/user-scripts/main/gitlab.js
 // @namespace    http://tampermonkey.net/
-// @version      0.0.7
+// @version      0.0.8
 // @description  TC
 // @author       TC
 // @homepage     https://github.com/typecoercion/user-scripts
