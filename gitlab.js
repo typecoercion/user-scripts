bdc31d7df0fc04817330c596eb6061b9d4132e60
Carbide Mouse <carbidemouse@gmail.com>
2023-07-27 12:24:58 -0400

    user-scripts

--- gitlab.js
+++ gitlab.js
@@ -3,7 +3,7 @@
 // @icon         https://assets.gitlab-static.net/assets/msapplication-tile-1196ec67452f618d39cdd85e2e3a542f76574c071051ae7effbfde01710eb17d.png
 // @downloadURL  https://raw.githubusercontent.com/typecoercion/user-scripts/main/gitlab.js
 // @namespace    http://tampermonkey.net/
-// @version      0.0.6
+// @version      0.0.7
 // @description  TC
 // @author       TC
 // @homepage     https://github.com/typecoercion/user-scripts
@@ -13,6 +13,23 @@
 // ==/UserScript==
 
 GM_addStyle(`
+
+.home-panel-title-row .avatar-container {
+  display: none;
+}
+
+div[data-testid="nav-container"] .gl-new-dropdown .gl-avatar {
+  margin-left: -20px;
+  opacity: 0;
+}
+
+.group-row-contents a:has([itemprop="logo"]) {
+  display: none;
+}
+
+.group-row-contents a:has([itemprop="image"]) {
+  display: none;
+}
 
 .breadcrumb-item-text img {
   display: none;
