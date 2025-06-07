a3cdb8b1abccde4efcc77ef3c081ebc75c49b834
Carbide Mouse <carbidemouse@gmail.com>
2025-06-07 00:30:34 -0400

    user-scripts

--- linear.js
+++ linear.js
@@ -1,10 +1,10 @@
 
 // ==UserScript==
 // @name         Linear
-// @icon         https://linear.app/static/apple-touch-icon.png
+// @icon         https://linear.app/cdn-cgi/imagedelivery/fO02fVwohEs9s9UHFwon6A/82d07241-84b3-4cdf-33b5-a09b8d169300/f=auto,dpr=2,q=95,fit=scale-down,metadata=none
 // @downloadURL  https://raw.githubusercontent.com/typecoercion/user-scripts/main/linear.js
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  TC
 // @author       TC
 // @homepage     https://github.com/typecoercion/user-scripts
