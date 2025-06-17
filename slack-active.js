5097eb159e9d85ab0af6648e4d886220de4a6609
Carbide Mouse <carbidemouse@gmail.com>
2025-06-17 13:45:55 -0400

    user-scripts

--- slack-active.js
+++ slack-active.js
@@ -1,6 +1,5 @@
 // ==UserScript==
 // @name         Slack: Active
-// @description  Keep Slack session active.
 // @icon         https://a.slack-edge.com/cebaa/img/ico/favicon.ico
 // @downloadURL  https://raw.githubusercontent.com/typecoercion/user-scripts/main/slack-active.js
 // @namespace    http://tampermonkey.net/
