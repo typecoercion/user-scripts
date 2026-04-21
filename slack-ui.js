5b0c361134c34a1dc1fd64d692f6682679e32e6f
Carbide Mouse <carbidemouse@gmail.com>
2026-04-21 18:13:08 -0400

    user-scripts

--- slack-ui.js
+++ slack-ui.js
@@ -3,7 +3,7 @@
 // @icon         https://a.slack-edge.com/cebaa/img/ico/favicon.ico
 // @downloadURL  https://raw.githubusercontent.com/typecoercion/user-scripts/main/slack-ui.js
 // @namespace    http://tampermonkey.net/
-// @version      1.2.3
+// @version      1.2.4
 // @description  TC
 // @author       TC
 // @homepage     https://github.com/typecoercion/user-scripts
@@ -42,12 +42,12 @@ setInterval(() => {
 
 GM_addStyle(`
 
-:root {
+/* :root {
   --x-height-add0: 40px;
   --x-height-add1: 40px;
   --x-height-min0: 0px;
   --x-height-min1: -4px;
-}
+} */
 
 a.p-upgrades_button,
 div[data-qa="upgrade-menu-item-wrapper"] {
@@ -65,8 +65,8 @@ button.p-account_switcher {
 
 body {
   font-size: 13px;
-  height: calc(100vh + var(--x-height-add1)) !important;
-  margin-top: calc(-1 * var(--x-height-add0)) !important;
+/*   height: calc(100vh + var(--x-height-add1)) !important;
+  margin-top: calc(-1 * var(--x-height-add0)) !important; */
 }
 
 div[data-sidebar-link-id="add_more_items_app"],
@@ -80,7 +80,7 @@ div.p-channel_sidebar--classic_nav {
   font-size: 13px;
 }
 
-div.p-ia4_client div.p-client_workspace__layout {
+/* div.p-ia4_client div.p-client_workspace__layout {
   border-radius: 6px 0 0 0;
   margin-right: var(--x-height-min1);
 }
@@ -88,7 +88,7 @@ div.p-ia4_client div.p-client_workspace__layout {
 div.p-ia4_sidebar_header.p-ia4_home_header:not(.p-ia4_home_header--setup,.p-ia4_home_header--with_guided_trial_badge) {
   --saf-0: rgba(var(--sk_foreground_low,29,28,29),.13);
   box-shadow: 0 1px 0 0 var(--saf-0);
-}
+} */
 
 div.p-ia4_upgrade_button__container {
   display: none;
@@ -102,9 +102,9 @@ div.p-workspace_banner__desktop-download-app {
   display: none;
 }
 
-div.ReactModal__Overlay div.ReactModal__Content:has(> div > .c-search_modal__wrapper) {
+/* div.ReactModal__Overlay div.ReactModal__Content:has(> div > .c-search_modal__wrapper) {
   top: 50px !important;
-}
+} */
 
 i.p-all_unreads_header_team_icon {
   display: none;
@@ -114,13 +114,13 @@ span.p-ia4_home_header_menu__team_name {
   display: none;
 }
 
-.p-ia4_client .p-client_workspace_wrapper {
+/* .p-ia4_client .p-client_workspace_wrapper {
   height: calc(100vh + 44px) !important;
 }
 
 .p-theme_background {
   height: calc(100vh + var(--x-height-add1)) !important;
-}
+} */
 
 .p-channel_sidebar__section_heading_button {
   text-transform: uppercase;
@@ -128,11 +128,11 @@ span.p-ia4_home_header_menu__team_name {
   font-size: 0.8em;
 }
 
-div.p-ia4_client div.p-view_contents--primary,
+/* div.p-ia4_client div.p-view_contents--primary,
 div.p-ia4_client div.p-view_contents--secondary,
 div.p-ia4_client div.p-view_contents--sidebar {
   max-height: 100vh;
-}
+} */
 
 .p-channel_sidebar:not(.p-channel_sidebar--visual-updates-m1) [data-show-avatars=false] .p-channel_sidebar__presence_icon.c-presence,
 .p-channel_sidebar__channel_icon_prefix {
